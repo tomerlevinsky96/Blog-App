ee9758171f3b309c71710185a5734082024e5dbb
tomerlevinsky96 <tomerlevinsky96@gmail.com>
2022-10-04 10:20:49 +0300

    cx

---
+++ pythonProject8/website/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,DateField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+
+    email = StringField('email',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+class SignUpForm(FlaskForm):
+
+    email = StringField('email',validators=[DataRequired()])
+    FirstName=StringField('FirstName', validators=[DataRequired()])
+    LastName = StringField('LastName', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    Birthday = DateField('Birthday', validators=[DataRequired()])
+
+class PostForm(FlaskForm):
+    text=StringField('text',validators=[DataRequired()])
+
+class commentForm(FlaskForm):
+    text=StringField('text',validators=[DataRequired()])
+
+
